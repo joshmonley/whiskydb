234bf50f174b56ce1c99c02dad4f6dc7edd10fbb
joshmonley <46253585+joshmonley@users.noreply.github.com>
2024-08-07 07:44:49 +1000

    removed swtich again

--- _data/frontend/src/App.js
+++ _data/frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import NewMakeTracking from './components/NewMakeTracking';
@@ -9,11 +9,11 @@ const App = () => (
   <Router>
     <div>
       <Navbar />
-      <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/new-make-tracking" component={NewMakeTracking} />
-        <Route path="/retail-whisky-collection" component={RetailWhiskyCollection} />
-      </Switch>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/new-make-tracking" element={<NewMakeTracking />} />
+        <Route path="/retail-whisky-collection" element={<RetailWhiskyCollection />} />
+      </Routes>
     </div>
   </Router>
 );
