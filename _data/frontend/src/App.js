87844b201d0ff684ee45c1f621d60a222941b216
joshmonley <46253585+joshmonley@users.noreply.github.com>
2024-08-10 18:41:18 +1000

    removed switch again!!!

--- _data/frontend/src/App.js
+++ _data/frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
 import NewMakeTracking from './components/NewMakeTracking';
 import RetailWhiskyCollection from './components/RetailWhiskyCollection';
